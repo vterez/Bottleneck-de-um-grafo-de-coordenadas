e647e9f5a56e0d79124683ec3ea32c43a6260dbf
Vitor Rezende <53321198+vterez@users.noreply.github.com>
2020-06-07 19:39:27 -0300

    First

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Telescopios_em_CSS
+{
+    class Program
+    {
+        static int Main(string [] args)
+        {
+            T2.Telescopio[] telesc;
+            int ncidades;
+			if (args.Length == 0)
+			{
+                Console.WriteLine("Arquivo nao fornecido");
+				return -1;
+			}
+            string arq = args[0];
+            T2.Funcoes.leentrada(arq, out telesc, out ncidades);
+            var pontes = T2.Funcoes.montaarestas(ncidades, telesc);
+            T2.Funcoes.radixsort(ncidades, pontes, telesc);
+            Console.WriteLine(T2.Funcoes.aPRIMorado(ncidades, telesc));
+            return 0;
+        }
+    }
+}
